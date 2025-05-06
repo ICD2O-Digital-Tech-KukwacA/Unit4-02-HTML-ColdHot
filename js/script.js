3cb6ae7a3c0f10f56c4ce852acab70014fbd4032
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-05-06 01:46:23 +0000

    Update 4 Unit4-02-HTML

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mr. Kukwac
+// Created on: May 2025
 // This file contains the JS functions for index.html
+"use strict"
+
+// This function checks if the temperature is hot or cold
+function tempCelsius() {
+    let display = "";
+    let givenTemp = 15
+    let userTemp = parseInt(document.getElementById("temp").value);
+    
+    if (userTemp >= givenTemp) {
+        display = "Its hot!";
+    }
+    
+    else {
+        display = "Its cold!";
+    }
+    document.getElementById("result").innerHTML = display; ;
+}
